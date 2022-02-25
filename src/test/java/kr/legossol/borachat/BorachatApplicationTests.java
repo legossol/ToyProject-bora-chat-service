f084e1cef78785e7c8a478f10826b66e6d39c479
장해솔 <hsjang@iparking.co.kr>
2022-02-25 14:06:22 +0900

    architecture set

---
+++ src/test/java/kr/legossol/borachat/BorachatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.legossol.borachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
